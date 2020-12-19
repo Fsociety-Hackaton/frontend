c98ffeec0b61bcd7fb03387ffeb65d3d89d335bc
martino2197 <martino2197@gmail.com>
2020-12-19 10:49:39 -0600

    LoginPage in /pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
 import React from "react";
-// import { LoginComponent } from "../components/LoginComponent";
+import fetch from "isomorphic-unfetch";
 
-const Home = () => {
+import { SearchComponent } from "../components/SearchComponent";
+// import { LoginComponent } from "../components/LoginComponent";
+import {
+  IndexContainer,
+  IndexHeader,
+  IndexLogo,
+  Favorites,
+} from "../components/PagesStyles/indexStyles";
+import { MainComponent } from "../components/MainComponent";
+import { GlobalStyles } from "../components/PagesStyles/GlobalStyles";
+
+const Home = (props) => {
   return (
-    <div>
-      <h1>Hola Platzi</h1>
-      {/* <LoginComponent /> */}
-    </div>
+    <>
+      <GlobalStyles />
+      <IndexContainer>
+        <IndexHeader>
+          <IndexLogo>
+            <img src="/image/logo2.png" alt="Platzi Logo" />
+          </IndexLogo>
+          <h1>Talent Placement</h1>
+          {console.log(props.offers)}
+          <Favorites>Favoritos</Favorites>
+        </IndexHeader>
+        <SearchComponent />
+        <MainComponent offers={props.offers} />
+      </IndexContainer>
+    </>
   );
 };
+
+Home.getInitialProps = async (ctx) => {
+  const res = await fetch("https://hackaton-master.herokuapp.com/jobs");
+  const resJSON = await res.json();
+  return {
+    offers: resJSON.data,
+  };
+};
+// console.log(props.users);
 
 export default Home;
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { LoginComponent } from "../../components/LoginComponent";
-import Image from "next/image";
+// import Image from "next/image";
 
 import {
   LoginContainer,
   LoginLogo,
 } from "../../components/PagesStyles/loginStyles";
+import { GlobalStyles } from "../../components/PagesStyles/GlobalStyles";
 
 // import Image from "../../assets/image";
 // const dev = require("/dev.jpg");
 
 const LoginPage = () => {
   return (
-    <LoginContainer>
-      <LoginLogo>
-        <img src="/image/logo2.png" alt="Platzi Logo" />
-      </LoginLogo>
-      <LoginComponent />
-    </LoginContainer>
+    <>
+      <GlobalStyles />
+      <LoginContainer>
+        <LoginLogo>
+          <img src="/image/logo2.png" alt="Platzi Logo" />
+        </LoginLogo>
+        <LoginComponent />
+      </LoginContainer>
+    </>
   );
 };
 
